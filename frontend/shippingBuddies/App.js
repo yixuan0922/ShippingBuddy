d51542ca10bdecee5ea4a563548226946c03778a
JinKang2000 <jinkanglim23@gmail.com>
2023-10-01 01:25:21 +0800

    ui
    ui

--- frontend/shippingBuddies/App.js
+++ frontend/shippingBuddies/App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ import HomeScreen from './screens/homescreen';
+ import DetailScreen from './screens/details';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+       <Stack.Navigator  initialRouteName="Details">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
   );
 }
 
-const styles = StyleSheet.create({
+const stylFes = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ frontend/shippingBuddies/screens/details.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+function DetailScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Detail Screen</Text>
+      </View>
+    );
+  }
+
+  export default DetailScreen
